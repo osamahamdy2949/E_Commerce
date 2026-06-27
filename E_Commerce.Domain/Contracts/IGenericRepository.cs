3bb183ba56dbe6296507f2f58c4326c019525b9b
Osama Hamdy <osama.hamdy2949@gmail.com>
2026-06-27 05:54:03 +0300

    Create Repository Pattern And Unit Of Work

---
+++ E_Commerce.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey> 
+    {
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+
+        Task<IReadOnlyList<TEntity>> GetAll(CancellationToken ct = default);
+        Task<TEntity?> GetById(TKey id, CancellationToken ct = default);
+    }
+}
---
+++ E_Commerce.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using E_Commerce.Domain.Common;
+using E_Commerce.Domain.Contracts;
+using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Infrastructure.Repositories
+{
+    internal class GenericRepository<TEntity, TKey>(StoreDbContext dbContext) : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        public void Add(TEntity entity) => dbContext.Set<TEntity>().Add(entity);
+
+        public void Delete(TEntity entity) => dbContext.Set<TEntity>().Remove(entity);
+
+        public async Task<IReadOnlyList<TEntity>> GetAll(CancellationToken ct = default)
+            => await dbContext.Set<TEntity>().ToListAsync(ct);
+
+        public async Task<TEntity?> GetById(TKey id, CancellationToken ct = default)
+            => await dbContext.Set<TEntity>().FindAsync(id, ct);
+
+        public void Update(TEntity entity) => dbContext.Set<TEntity>().Update(entity);
+    }
+}
